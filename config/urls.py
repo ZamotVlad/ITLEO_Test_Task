6d28b041d3bc88c8c6c2c5423de582981661cf84
Vladyslav Zamotailo <zamotvlad@gmail.com>
2026-06-15 02:57:09 +0300

    feat: add DRF router and Swagger urls

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,31 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from rest_framework.routers import DefaultRouter
+
+from payments.views import PaymentViewSet
+from schedule.views import GroupViewSet, ScheduleViewSet
+from students.views import CourseViewSet, StudentViewSet
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet, basename="course")
+router.register(r"students", StudentViewSet, basename="student")
+router.register(r"groups", GroupViewSet, basename="group")
+router.register(r"schedule", ScheduleViewSet, basename="schedule")
+router.register(r"payments", PaymentViewSet, basename="payment")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
