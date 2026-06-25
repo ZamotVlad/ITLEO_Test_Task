05e910cc0b763ce1374aa2e43f5759e61332dd02
Vladyslav Zamotailo <zamotvlad@gmail.com>
2026-06-25 15:14:57 +0300

    feat: add ExportMixin to UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from import_export.admin import ExportMixin
 from unfold.admin import ModelAdmin
 
-from .models import User
+from accounts.models import User
+from accounts.roles import OPERATIONAL_ROLES, Roles
 
 
 @admin.register(User)
-class CustomUserAdmin(ModelAdmin, UserAdmin):
+class CustomUserAdmin(ExportMixin, ModelAdmin, UserAdmin):
     list_display = ("username", "email", "role", "is_staff", "is_active")
     list_filter = ("role", "is_staff")
-    fieldsets = UserAdmin.fieldsets + (("Роль", {"fields": ("role",)}),)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Роль і Telegram", {"fields": ("role", "telegram_chat_id")}),
+    )
+
+    def has_export_permission(self, request):
+        return request.user.role in OPERATIONAL_ROLES
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.role == Roles.OWNER
